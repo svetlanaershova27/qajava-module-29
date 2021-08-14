7cef88adee96d3d8a0c567dd601fe1f1950ec73b
svetlanaershova <svetl.ershova2014@yandex.ru>
2021-08-15 01:14:18 +0300

    создали TeenageTest

---
+++ maven_ext/src/test/java/PersonTest.java
@@ -0,0 +1,54 @@
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.testng.Assert.assertEquals;
+
+public class PersonTest {
+
+    public void currentTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+    }
+
+    @BeforeClass
+    void BeforeClass() {
+        System.out.println("TeenageTest test started");
+        currentTime();
+    }
+
+    @AfterClass
+    void AfterClass() {
+        System.out.println("TeenageTest test done");
+        currentTime();
+    }
+
+    @DataProvider(name = "ageTest")
+    Object[][] ageDataProvider() {
+        return new Object[][]{
+                {10, false},
+                {0, false},
+                {16, true},
+                {20, false},
+                {17, true},
+                {-10, false},
+        };
+    }
+
+    @Test(dataProvider = "ageTest")
+    void TestIsTeenager(int age, boolean expectedResult) {
+
+        boolean result = Person.isTeenager(age);
+        System.out.println(result);
+        assertEquals(result, expectedResult,age + "  - не тинейджер, тинейджер: 13-19");
+
+    }
+
+}
